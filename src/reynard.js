7a78afe30dd6d57b6ee2b1c1bb3d9e5940c16e3f
TechCPT <paktungchow04@gmail.com>
2020-12-20 14:29:26 -0500

    Updated repository

--- src/reynard.js
+++ src/reynard.js
@@ -1,1 +1,33 @@
-console.log('Hello world');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv-flow').config();
+
+client.once('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (message) => {
+	if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
+
+	if (message.content.startsWith (process.env.PREFIX + 'myping')) {
+		message.channel.send('Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
+	}
+
+	if (message.content === `${process.env.PREFIX}serverinfo`) {
+		message.channel.send(`**Server name:** ${message.guild.name}\n**Total members:** ${message.guild.memberCount}`);
+	}
+
+	if (message.content === `${process.env.PREFIX}userinfo`) {
+		message.channel.send(`**Your username:** ${message.author.username}\n**Your ID:** ${message.author.id}`);
+	}
+
+	if (message.content.startsWith (process.env.PREFIX + 'info')) {
+		message.reply ('Hey there! I\'m Reynard, a Discord bot created with Node.js.');
+	}
+	if (message.content == process.env.PREFIX + 'hello') {
+		message.channel.send('Hey!');
+	}
+});
+
+
+client.login(process.env.BOT_TOKEN);
