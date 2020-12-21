9cfce460de2e99260600a4bb48d51604949fdf26
TechCPT <paktungchow04@gmail.com>
2020-12-21 16:30:33 -0500

    Update reynard.js

--- src/reynard.js
+++ src/reynard.js
@@ -2,8 +2,8 @@ require('dotenv-flow').config();
 const Client = require('./client/Client');
 new Client().start(process.env.BOT_TOKEN, './commands', './events');
 
-client.on('ready', () => {
-	client.user.setPresence({
+Client.on('ready', () => {
+	Client.user.setPresence({
 		game: {
 			name: 'my code',
 			type: 'WATCHING',
