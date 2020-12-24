3b7c2966c44860be767402cb768948ba96126c57
TechCPT <paktungchow04@gmail.com>
2020-12-24 14:55:01 -0500

    Update repository

---
+++ src/commands/delete.js
@@ -0,0 +1,15 @@
+const Client = require('../structures/Client');
+const { Message } = require('discord.js');
+
+module.exports = {
+	name: 'delete',
+	/**
+     * @param {Message} message
+     * @param {Client} client
+     * @param {String[]} args
+     */
+	run: async (client, message, args) => {
+		message.channel.bulkDelete(args);
+	},
+	aliases: ['del'],
+};
