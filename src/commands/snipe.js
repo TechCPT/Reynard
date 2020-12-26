3254bc3d7c8f2c1ba028b40fdab807932e437c35
TechCPT <paktungchow04@gmail.com>
2020-12-25 19:03:09 -0500

    Update repo

--- src/commands/snipe.js
+++ src/commands/snipe.js
@@ -11,7 +11,7 @@ module.exports = {
 	run: async (client, message, args) => {
 		const msg = client.snipes.get(message.channel.id);
 		if(!msg) return message.channel.send("There are no deleted messages in this channel!");
-		const embed = new Discord.MessageEmbed()
+		const embed = new MessageEmbed()
 			.setAuthor(msg.author)
 			.setDescription(msg.content);
 		if(msg.image)embed.setImage(msg.image);
