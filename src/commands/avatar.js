ca696792fb9d43d8b4f8053e619868a61433ed41
TechCPT <paktungchow04@gmail.com>
2021-03-17 18:40:50 -0400

    Added avatar cmd

---
+++ src/commands/avatar.js
@@ -0,0 +1,25 @@
+const Client = require("../handlers/Client");
+const { Message, MessageEmbed } = require("discord.js");
+
+module.exports = {
+	name: "avatar",
+	/**
+     * @param {Message} message
+     * @param {Client} client
+     * @param {String[]} args
+     */
+	run: async (client, message, args) => {
+
+		const member = message.mentions.users.first() || message.author;
+
+		const avatar = member.displayAvatarURL({ dynamic: true, size: 1024 });
+
+		const msg = message.channel.send(
+			new MessageEmbed()
+				.setTitle(`${member.username}'s avatar`)
+				.setColor("ORANGE")
+				.setImage(avatar)
+				.setFooter("© 2021 Reynard | Visit the Reynard Official Discord server for more information!"),
+		);
+	},
+};
