5a8923412703c49420a4a9e3863890d42a4f2d11
TechCPT <paktungchow04@gmail.com>
2021-03-22 06:09:35 -0400

    Added membercount cmd

---
+++ src/commands/memberCount.js
@@ -0,0 +1,29 @@
+const Client = require("../handlers/Client");
+const { Message, MessageEmbed, Constants } = require("discord.js");
+
+const dayjs = require("dayjs");
+
+module.exports = {
+	name: "membercount",
+	/**
+     * @param {Message} message
+     * @param {Client} client
+     * @param {String[]} args
+     */
+	run: async (client, message, args) => {
+		const { guild } = message;
+
+
+		const msg = message.channel.send(
+			new MessageEmbed()
+				.setColor("ORANGE")
+				.addFields(
+					{ name: "Members", value: message.guild.memberCount, inline: true },
+					{ name: "Humans", value: message.guild.members.cache.filter(m => !m.user.bot).size, inline: true },
+					{ name: "Bots", value: message.guild.members.cache.filter(m => m.user.bot).size, inline: true },
+				)
+				.setFooter(`${new Date()}`),
+		);
+	},
+	aliases: ["members"],
+};
