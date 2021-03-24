6052aff68c5394d0011c45a15aed1b5c7bf3d46c
TechCPT <paktungchow04@gmail.com>
2021-03-23 23:07:04 -0400

    Attempted to fix cmd bug

--- src/commands/guildLeave.js
+++ src/commands/guildLeave.js
@@ -13,19 +13,21 @@ module.exports = {
 
 		const id = args[0];
 
-		const icon = guild.iconURL();
-
 		client.guilds.cache.get(id).leave()
-			.then(g => message.channel.send(
-				new MessageEmbed()
-					.setAuthor(guild.name, guild.iconURL({ format: "png", dynamic: true }))
-					.setDescription(`Successfully left ${guild.name}!`)
-					.setThumbnail(icon)
-					.setColor("ORANGE")
-					.setFooter(`ID: ${id}`)
-					.setTimestamp(`${new Date()}`),
-			)
-				.catch(err => console.log(`There was an error leaving the guild: \n ${err}`)));
+			.then(guild => {
+				const icon = guild.iconURL();
+
+				message.channel.send(
+					new MessageEmbed()
+						.setAuthor(guild.name, guild.iconURL({ format: "png", dynamic: true }))
+						.setDescription(`Successfully left ${guild.name}!`)
+						.setThumbnail(icon)
+						.setColor("ORANGE")
+						.setFooter(`ID: ${id}`)
+						.setTimestamp(`${new Date()}`),
+				);
+			})
+			.catch(err => console.log(`There was an error leaving the guild: \n ${err}`));
 
 	},
 	aliases: ["gleave"],
