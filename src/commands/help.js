01703c7d618633dcb8ed2e390d2c6d4975cd553e
TechCPT <paktungchow04@gmail.com>
2021-03-25 18:49:51 -0400

    Update help cmd

--- src/commands/help.js
+++ src/commands/help.js
@@ -12,24 +12,30 @@ module.exports = {
 	run: async (client, message, args) => {
 		const msg = message.channel.send(
 			new MessageEmbed()
-				.setTitle("Reynard HELP")
+				.setAuthor(client.user.tag, client.user.displayAvatarURL({ format: "png", dynamic: true }))
+				.setTitle("**Prefix:** `r!`")
 				.setColor("ORANGE")
 				.setDescription(stripIndents`
 					Reynard is a multipurpose Discord bot built with Node.js.
-					
+
+					Reynard currently has **8** public commands available!
+
 					Reynard Official Discord — https://discord.gg/heHzCu3mx8
 					
 					**List of Reynard Bot Commands**
 					\`r!avatar\` — Displays the avatar image of a user.
 					\`r!commands\` – COMING SOON!
 					\`r!delete [# of messages]\` – Deletes the inputted amount of messages along with the command used.
+					\`r!help\` — Displays information about the bot.
+					\`r!invite\` — Reynard's invite link.
 					\`r!membercount\` — Displays the membercount of the current server.
 					\`r!ping\` – Shows the bot's current ping.
 					\`r!serverinfo\` — Displays information on the current server.
 					\`r!snipe\` – Reveals the most recently deleted message.
 					`,
 				)
-				.setFooter("© 2021 Reynard | Visit the Reynard Official Discord server for more information!"),
+				.setFooter("© 2021 Reynard")
+				.setTimestamp(`${new Date()}`),
 		);
 	},
 };
